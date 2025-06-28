42a46af618c4b6efa949a6199f4c11f73d327df3
ShanySB <shanyb.sb@gmail.com>
2025-06-28 23:08:16 +0100

    destructing

---
+++ destructuring.js
@@ -0,0 +1,25 @@
+/**
+ * To run this file in GitPod, use the
+ * command node destructuring.js in the terminal
+ */
+
+// Destructuring arrays
+ let ages = [30, 26, 27];
+// let john = ages[0];
+// let mary = ages[1];
+// let joe = ages[2];
+let [john, mary, joe] = ages;
+console.log(john, mary, joe);
+
+// Destructuring objects
+let jobs = {
+    mike: "designer",
+    jill: "developer",
+    alicia: "accountant",
+};
+let { mike, jill, alicia } = jobs;
+console.log(mike, jill, alicia);
+
+// Destructuring subsets
+
+// using rest parameter syntax
